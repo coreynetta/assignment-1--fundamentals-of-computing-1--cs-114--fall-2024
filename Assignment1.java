c1a9b90853b3b4a1e093d14bd06b06e1c6c4e9f5
corey <coreythehop@gmail.com>
2024-09-18 17:23:55 -0400

    Created Comment

--- Assignment1.java
+++ Assignment1.java
@@ -10,5 +10,23 @@ public class Assignment1 {
     System.out.println(" CC            HHHH    HHHH") ;
     System.out.println(" CCCC          HHHH    HHHH") ;
     System.out.println("   CCCCCCC     HHHH    HHHH") ;
+
+    // int BASE = fahrenheit;
+    // double celsius;
+
+    // Scanner scan = new Scanner(System.in);
+
+    // System.out.print("Enter the number in Fahrenheit: ");
+    // fahrenheit = scan.nextInt();
+
+    // final double CONVERSION_FACTOR = 9.0 / 5.0;
+
+    // double fahrenheit;
+    // int celsius = 24;
+
+    // fahrenheit = celsius * CONVERSION_FACTOR + 32;
+
+    // System.out.println("Celsius Temperature: " + celsiusTemp);
+    // System.out.println("Fahrenheit Equivalent: " + fahrenheitTemp);
   }
 }
