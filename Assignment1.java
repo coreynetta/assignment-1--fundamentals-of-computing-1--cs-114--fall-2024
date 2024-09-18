a1a93d07087d782077bbc844322a3c6ba8d45104
corey <coreythehop@gmail.com>
2024-09-18 18:27:04 -0400

    Removed Additional Comments I made for Notation

--- Assignment1.java
+++ Assignment1.java
@@ -16,34 +16,14 @@ public class Assignment1 {
 
     // Formula: F = (9/5) * C + 32
 
-    // int BASE = fahrenheit;
-    // double celsius;
-
     System.out.print("Enter the number in Fahrenheit: ");
     double fahrenheit = scan.nextDouble();
 
-    // (ORIGINAL) final double CONVERSION_FACTOR = 9.0 / 5.0;
 
     double celsius = (fahrenheit - 32) * 5 / 9;
 
     System.out.println("Celsius Temperature: " + celsius);
     System.out.println("Fahrenheit Equivalent: " + fahrenheit);
-    // START OF NEXT PORTION
-
-    // Scanner scanner = new Scanner(System.in);
-    // Random random = new Random();
-
-    // System.out.print("Enter a 5-character string: ");
-    // String input = scan.nextLine();
-
-    // String middlePart = input.substring(1, 4);
-    // String reversedMiddlePart = new
-    // StringBuilder(middlePart).reverse().toString();
-
-    // int randomNumber = 32 + random.nextInt(16384 - 32 + 1);
-
-    // System.out.println("Reversed middle part of the string: " + reversedMiddlePart);
-    // System.out.println("Random number between 32 and 16,384: " + randomNumber);
 
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
@@ -59,7 +39,6 @@ public class Assignment1 {
     System.out.println("Reversed middle part of the string: " + reversedMiddlePart);
     System.out.println("Random number between 32 and 16,384: " + randomNumber);
 
-    // end result
 
     System.out.println("Combined output: " + celsius + reversedMiddlePart + randomNumber);
 
