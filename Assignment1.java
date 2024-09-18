de33dc1844e4e3c2ec33441aac4060f253484a90
corey <coreythehop@gmail.com>
2024-09-18 17:19:59 -0400

    Print out initials

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,13 @@ Import java.util.Scanner
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("   CCCCCCC     HHHH    HHHH") ;
+    System.out.println(" CCCC          HHHH    HHHH") ;
+    System.out.println(" CC            HHHH    HHHH") ;
+    System.out.println(" CC            HHHHHHHHHHHH") ;
+    System.out.println(" CC            HHHHHHHHHHHH") ;
+    System.out.println(" CC            HHHH    HHHH") ;
+    System.out.println(" CCCC          HHHH    HHHH") ;
+    System.out.println("   CCCCCCC     HHHH    HHHH") ;
   }
 }
